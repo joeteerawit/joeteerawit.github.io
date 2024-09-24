2d3da6c6e0261b57660efb7a0e072b7619fe7975
Joewalker <teerawit@hashpays.io>
2024-09-24 17:18:24 +0700

    edit home page

--- docs/.vuepress/client.js
+++ docs/.vuepress/client.js
@@ -3,10 +3,12 @@ import Article from './layouts/Article.vue'
 import Category from './layouts/Category.vue'
 import Tag from './layouts/Tag.vue'
 import Timeline from './layouts/Timeline.vue'
+import HomeLayout from './layouts/HomeLayout.vue'
 
 export default defineClientConfig({
   // we provide some blog layouts
   layouts: {
+    HomeLayout,
     Article,
     Category,
     Tag,
---
+++ docs/.vuepress/layouts/HomeLayout.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="home-layout">
+    <nav class="navbar">
+      <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/guide/">Guide</a></li>
+        <li><a href="/about/">About</a></li>
+      </ul>
+    </nav>
+    <main class="content">
+      <h1>Hello</h1>
+    </main>
+  </div>
+</template>
+
+<style scoped>
+.home-layout {
+  text-align: center;
+  margin: 20px;
+}
+
+.navbar {
+  background-color: #333;
+  padding: 10px;
+}
+
+.navbar ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+.navbar ul li {
+  display: inline;
+  margin-right: 20px;
+}
+
+.navbar ul li a {
+  color: white;
+  text-decoration: none;
+}
+
+.content {
+  margin-top: 40px;
+}
+</style>
