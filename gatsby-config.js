b107ecd507b811514f251930ecd1f52c18583dd6
joewalker <teerawit@hashpays.io>
2024-11-01 23:02:52 +0700

    add favicon icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,21 @@
 module.exports = {
   siteMetadata: {
     title: `joeteerawit.github.io`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: ["gatsby-plugin-postcss"]
-};
+  plugins: [
+    'gatsby-plugin-postcss',
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Joeteerawit`,
+        short_name: `J`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/logo2.png`
+      },
+    },
+  ],
+}
