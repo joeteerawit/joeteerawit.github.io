69a031d684c97a14fa31ce1c32f9ed02b81aabb5
joewalker <assassin.joe@hotmail.come>
2025-04-22 00:29:36 +0700

    create sidebar with test book view

--- src/content.config.ts
+++ src/content.config.ts
@@ -19,19 +19,4 @@ const blog = createCollection('./src/content/blog')
 const book = createCollection('./src/content/book')
 const tutorial = createCollection('./src/content/tutorial')
 
-// const blog = defineCollection({
-//   // Load Markdown and MDX files in the `src/content/blog/` directory.
-//   loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
-//   // Type-check frontmatter using a schema
-//   schema: z.object({
-//     title: z.string(),
-//     description: z.string(),
-//     // Transform string to Date object
-//     pubDate: z.coerce.date(),
-//     updatedDate: z.coerce.date().optional(),
-//     heroImage: z.string().optional(),
-//     draft: z.boolean().optional(),
-//   }),
-// })
-
 export const collections = { blog, book, tutorial }
