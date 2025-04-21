bec5509c04ca20f961b04509011253184ae0c8c1
Joewalker <teerawit@hashpays.io>
2025-04-21 20:49:08 +0700

    update content & install tailwindcss

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,19 +1,37 @@
 import { glob } from 'astro/loaders'
 import { defineCollection, z } from 'astro:content'
 
-const blog = defineCollection({
-  // Load Markdown and MDX files in the `src/content/blog/` directory.
-  loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
-  // Type-check frontmatter using a schema
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    // Transform string to Date object
-    pubDate: z.coerce.date(),
-    updatedDate: z.coerce.date().optional(),
-    heroImage: z.string().optional(),
-    draft: z.boolean().optional(),
-  }),
-})
+const createCollection = (basePath: string) => {
+  return defineCollection({
+    loader: glob({ base: basePath, pattern: '**/*.{md,mdx}' }),
+    schema: z.object({
+      title: z.string(),
+      description: z.string(),
+      pubDate: z.coerce.date(),
+      updatedDate: z.coerce.date().optional(),
+      heroImage: z.string().optional(),
+      draft: z.boolean().optional(),
+    }),
+  })
+}
 
-export const collections = { blog }
+const blog = createCollection('./src/content/blog')
+const book = createCollection('./src/content/book')
+const tutorial = createCollection('./src/content/tutorial')
+
+// const blog = defineCollection({
+//   // Load Markdown and MDX files in the `src/content/blog/` directory.
+//   loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
+//   // Type-check frontmatter using a schema
+//   schema: z.object({
+//     title: z.string(),
+//     description: z.string(),
+//     // Transform string to Date object
+//     pubDate: z.coerce.date(),
+//     updatedDate: z.coerce.date().optional(),
+//     heroImage: z.string().optional(),
+//     draft: z.boolean().optional(),
+//   }),
+// })
+
+export const collections = { blog, book, tutorial }
