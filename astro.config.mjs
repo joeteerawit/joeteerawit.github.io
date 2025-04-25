ed9b5f80614a669e3acd2bd7da91c31836ba3f2c
joewalker <assassin.joe@hotmail.come>
2025-04-25 23:00:14 +0700

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
-  base: './',
   site: 'https://joewalker.xzy',
   integrations: [mdx(), sitemap()],
   vite: {
