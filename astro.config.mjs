009267c9178989647f6e2376328f7e0dc58cef7b
joewalker <assassin.joe@hotmail.come>
2025-04-27 12:00:15 +0700

    adjust css, install mermaidjs, code theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,32 @@
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 import tailwindcss from '@tailwindcss/vite'
+import rehypeMermaid from 'rehype-mermaid'
+
+import { transformerMetaHighlight, transformerNotationDiff, transformerNotationFocus } from '@shikijs/transformers'
 import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
+// https://docs.astro.build/en/guides/styling/#markdown-styling
 export default defineConfig({
   site: 'https://www.joewalker.xzy',
   integrations: [mdx(), sitemap()],
+  markdown: {
+    syntaxHighlight: {
+      type: 'shiki',
+      excludeLangs: ['mermaid', 'math'],
+    },
+    rehypePlugins: [rehypeMermaid],
+    shikiConfig: {
+      theme: 'github-light',
+      wrap: true,
+      transformers: [
+        transformerMetaHighlight(),
+        transformerNotationDiff(),
+        transformerNotationFocus(),
+      ],
+    },
+  },
   vite: {
     plugins: [tailwindcss()],
     build: {
@@ -18,6 +38,6 @@ export default defineConfig({
     },
   },
   build: {
-    inlineStylesheets: 'always', 
+    inlineStylesheets: 'always',
   },
 })
