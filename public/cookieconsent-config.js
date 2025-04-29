38f7922f80a2afa0119ea6cc80708aa8a360c5d7
joewalker <assassin.joe@hotmail.come>
2025-04-29 09:51:15 +0700

    fix google analytic

--- public/cookieconsent-config.js
+++ public/cookieconsent-config.js
@@ -1,8 +1,5 @@
 import 'https://cdn.jsdelivr.net/gh/orestbida/cookieconsent@3.1.0/dist/cookieconsent.umd.js';
 
-// Enable dark mode
-document.documentElement.classList.add('cc--darkmode');
-
 CookieConsent.run({
     guiOptions: {
         consentModal: {
